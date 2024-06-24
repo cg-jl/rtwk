a902b12bc827e9f9c54549a3fd0d2fe03519687b
Jesús Lapastora <cyber.gsuscode@gmail.com>
2024-06-24 22:18:27 +0200

    Remove spatially_bounded struct
    It achieves nothing.

--- src/geometry.h
+++ src/geometry.h
@@ -14,11 +14,8 @@ struct uvs {
     double u, v;
 };
 
-struct spatially_bounded {
+struct geometry {
     virtual aabb bounding_box() const = 0;
-};
-
-struct geometry : public spatially_bounded {
     virtual bool hit(ray const &r, interval ray_t,
                      geometry_record &rec) const = 0;
     virtual void getUVs(uvs &uv, point3 intersection, vec3 normal) const = 0;
